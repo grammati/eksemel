1607fb937841407151d330589b1d3c0145d95079
Chris Perkins <chrisperkins99@gmail.com>
2011-12-18 16:13:05 -0500

    starting to add tests. 1 down, 92381 to go. Includes a test that can't pass until I push :)

--- test/eksemel/test/xml.clj
+++ test/eksemel/test/xml.clj
@@ -24,6 +24,26 @@
       (xml-str= a b))))
 
 
+;;; Parsing
+(deftest parse-sources
+  (testing "parse works with a file, URL, string, etc..."
+    (doseq [source ["./test/resources/sample-1.xml"
+                    "https://raw.github.com/grammati/eksemel/master/test/resource/sample-1.xml"
+                    (-> "<foo/>" java.io.StringReader.)
+                    (-> "<foo/>" java.io.StringReader. org.xml.sax.InputSource.)
+                    (-> "<foo/>" (.getBytes (java.nio.charset.Charset/forName "utf-8")))
+                    (-> "<foo/>" (.getBytes (java.nio.charset.Charset/forName "utf-8"))
+                                 java.io.ByteArrayInputStream.)
+                    ;; Can also parse a string directly (but I may change it to
+                    ;; parse-str instead - can a file name ever start with "<"?).
+                    "<foo/>"]]
+      (let [parsed (xml/parse source)]
+        (is (= 1 (count parsed)))
+        (is (xml/element? (first parsed)))
+        (is (= :foo (xml/tag (first parsed))))
+        (is (nil? (xml/uri (first parsed))))
+        ))))
+
 (def xml-1 "<a><b/></a>")
 (def xml-2 "<a a1='v1'><b a2='v2'/></a>")
 
